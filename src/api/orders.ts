af4b9b44f9ab3bfd8c9910ccf654b2b548b3e91c
Gustavo Marconi <44904642+gutomarconi@users.noreply.github.com>
2023-11-13 16:01:34 -0300

    Update orders.ts

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -5,7 +5,7 @@ export const fetchOrders = async () => {
   const token = process.env.API_TOKEN;
   const baseDomain = process.env.BACKEND_API_DOMAIN;
   const productApi = 'pedidos.pesquisa.php';
-  const url = `${baseDomain}${productApi}?formato=json&token${token}&marcador=${PDV_MARKER}`;
+  const url = `${baseDomain}${productApi}?formato=json&token=${token}&marcador=${PDV_MARKER}`;
 
   const { data } = await axios.post(url);
 
@@ -17,7 +17,7 @@ export const getOrder = async (id: number) => {
   const token = process.env.API_TOKEN;
   const baseDomain = process.env.BACKEND_API_DOMAIN;
   const productApi = 'pdv.pedido.obter.php';
-  const url = `${baseDomain}${productApi}?formato=json&token${token}&id=${id}`;
+  const url = `${baseDomain}${productApi}?formato=json&token=${token}&id=${id}`;
 
   const { data } = await axios.post(url);
 
