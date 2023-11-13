543a889f6b1e015ef4189421fec2fe4be837c636
Gustavo Marconi <44904642+gutomarconi@users.noreply.github.com>
2023-11-13 16:01:14 -0300

    Update items.ts

--- src/api/items.ts
+++ src/api/items.ts
@@ -5,7 +5,7 @@ export const fetchItems = async (idTag: TagIds) => {
   const token = process.env.API_TOKEN;
   const baseDomain = process.env.BACKEND_API_DOMAIN;
   const productApi = 'produtos.pesquisa.php';
-  const url = `${baseDomain}${productApi}?formato=json&token${token}&idTag=${idTag}`;
+  const url = `${baseDomain}${productApi}?formato=json&token=${token}&idTag=${idTag}`;
 
   const { data } = await axios.post(url);
 
